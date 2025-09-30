7d2cf667dfa2e72629838dd7160758d67fcd573d
SimoneMac <simonepersonale@AirdiSimoneMac.Home>
2025-10-01 01:35:15 +0200

    fix build

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = { reactStrictMode: true };
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',   // <-- nuova sintassi per static export
+  images: {
+    unoptimized: true, // utile se usi <Image> di Next
+  },
+};
+
+module.exports = nextConfig;
+
