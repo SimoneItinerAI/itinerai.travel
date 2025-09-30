48f3ee8ee83e0a2a50e11501c0a5b63e4928aa7f
SimoneMac <simonepersonale@AirdiSimoneMac.Home>
2025-10-01 01:20:49 +0200

    cambio nomi

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 // pages/index.js
 import Head from "next/head";
-import Hero from "../components/Hero.js";
+import Hero from "../components/Hero";
 import Newsletter from "../components/Newsletter";
 
 export default function Home({ show }) {
-return (
-<>
-<Head>
-<title>ItinerAI – Itinerari di viaggio intelligenti</title>
-<meta name="description" content="Trova voli, hotel ed esperienze con l'aiuto di ItinerAI. Pianificazione semplice e veloce." />
-<meta property="og:title" content="ItinerAI – Itinerari di viaggio intelligenti" />
-<meta property="og:description" content="Trova voli, hotel ed esperienze con l'aiuto di ItinerAI." />
-<meta property="og:image" content="/hero-bg.jpg" />
-</Head>
-<Hero show={show} />
-<Newsletter />
-</>
-);
+  return (
+    <>
+      <Head>
+        <title>ItinerAI – Itinerari di viaggio intelligenti</title>
+        <meta name="description" content="Trova voli, hotel ed esperienze con l'aiuto di ItinerAI" />
+        <meta property="og:title" content="ItinerAI – Itinerari di viaggio intelligenti" />
+        <meta property="og:description" content="Trova voli, hotel ed esperienze con l'aiuto di ItinerAI." />
+        <meta property="og:image" content="/hero-bg.jpg" />
+      </Head>
+
+      <Hero show={show} />
+      <Newsletter />
+    </>
+  );
 }
