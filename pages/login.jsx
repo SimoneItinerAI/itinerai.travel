c2ebdd026562ddb95fbe95eb2450e577c0005e31
SimoneItinerAI <simonesammarco01@gmail.com>
2025-11-12 23:57:39 +0100

    Delete pages directory

--- pages/login.jsx
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-  return (
-    <div style={{ padding: "100px 20px", color: "#fff", background: "#0f172a", minHeight: "100vh" }}>
-      <h1>Accedi / Registrati</h1>
-      <p>Area riservata in arrivo.</p>
-    </div>
-  );
-}
--- pages/prenota.jsx
+++
@@ -1,8 +0,0 @@
-export default function Prenota() {
-  return (
-    <div style={{ padding: "100px 20px", color: "#fff", background: "#0f172a", minHeight: "100vh" }}>
-      <h1>Prenota</h1>
-      <p>Presto potrai prenotare voli, hotel ed esperienze direttamente da ItinerAI.</p>
-    </div>
-  );
-}
